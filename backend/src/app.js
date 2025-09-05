55f33c37329c2f80596e20f4d0450fe5c6e576dd
Ashutosh Atoliya <ashutoshatoliya@gmail.com>
2025-09-05 22:07:52 +0530

    user router

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const cookieParser = require("cookie-parser");
 const express = require("express");
 
+const  userRouter = require("./routes/user.router");
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
 
+app.use("/api/users", userRouter);
 module.exports = app;
