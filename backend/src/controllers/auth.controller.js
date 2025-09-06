684e504d7d7fc3b01c3ffe8b439f136d1e4732c7
Ashutosh Atoliya <ashutoshatoliya@gmail.com>
2025-09-06 05:56:17 +0530

    user auth complete

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -79,6 +79,48 @@ const registerUser = async (req, res) => {
   }
 };
 
+const loginUser = async(req, res)=>{
+  const {email, username, password} = req.body;
+
+  const user = await userModel.findOne({
+    $or:[
+      {username}, {email}
+    ]
+  }).select("+password");
+
+
+  if(!user){
+    return res.status(400).json({
+      message:"Invalid Username or email"
+    })
+  };
+
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if(!isPasswordValid){
+    return res.status(400).json({
+      message:"invalid password"
+    })
+  };
+
+const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+
+  res.cookie("token", token);
+
+  res.status(200).json({
+    message:"User Logged in Successfully",
+    user:{
+      id: user._id,
+      username: user.username,
+      email: user.email,
+      fullName:user.fullName,
+      role: user.role
+    }
+  })
+}
+
 module.exports = {
   registerUser,
+  loginUser
 };
